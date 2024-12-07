de34092f8ad35c80f7079a142e36038464aa66b4
mekel <mekelcruzz@gmail.com>
2024-12-07 23:45:18 +0800

    kangkongchips

--- lolos-place/backend/server.js
+++ lolos-place/backend/server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const PAYMONGO_SECRET_KEY = 'sk_test_Uarb4zRpXZb9PXmTHeK1ZTEp';
 const axios = require('axios');
 
-// update 1155asd
+// kangkongchips
 
 const app = express();
 app.use(cors());
